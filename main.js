0d200b2e4a7d76ab1bbf2402daf9df4fee5e2b0b
AJ <j.aravindan@gmail.com>
2014-07-26 20:28:07 +0530

    26July14
    Commit from windows

--- main.js
+++ main.js
@@ -1,81 +1,63 @@
 global.$ = $;
 
+var jade = require('jade');
 var course = require('course');
-var login = require('login');
+
+var __viewdir = "./views/";
+
+var Datastore = require('nedb')
+  , path = require('path')
+  , db = new Datastore({ filename: path.join(require('nw.gui').App.dataPath, 'local.db') });
 
 $(document).ready(function() {
 
-  login.init_app();
+  initDS();
 
-});
+  if(!sessionStorage['user']){
 
+    $('#content').append(jade.renderFile(__viewdir + './userlogin.jade'));
 
-function processForm(e) {
-  if (e.preventDefault) e.preventDefault();
-  var form = document.getElementById('login-form');
-  login.loginuser(form.username.value, form.password.value);
+    var form = document.getElementById('login-form');
+    if (form.attachEvent) {
+      form.attachEvent("submit", processLogin);
+    } else {
+      form.addEventListener("submit", processLogin);
+    }
 
-  return false;
-}
+  }else{
 
-
-function chkuser(){
-
-  if (login.loginuser()){
-
-    $('#content').html = "";
     $('#content').append(course.render());
 
   }
 
+});
+
+function processLogin(e) {
+  if (e.preventDefault) e.preventDefault();
+  var form = document.getElementById('login-form');
+  //console.log('Before find...');
+  db.find({ $and: [{user: form.username.value}, {password: form.password.value}]}, function (err, docs) {
+  //               console.log(err);
+                  if(docs){
+                    $('#content').append(course.render());
+                  } else {
+                    $('#content').append(jade.renderFile(__viewdir + './userlogin.jade', {error:'User doesnot exist.'}));
+                  }
+
+                });
+
+  return false;
 }
 
-var video_source = new Array();
-var audio_source = new Array();
+function initDS(){
 
-/*function failure(e) {
-  console.log("Failure");
-  alert(e.name)
-}*/
+  var doc = { user: 'demo',
+             password: 'demo',
+             points: null,
+             coursePending: ['course001.json'],
+             courseCompleted: [{course_id: '', course_file: ''}]
+            };
 
-function start() {
-  MediaStreamTrack.getSources(gotSources);
+  db.insert(doc);
+  db.ensureIndex({ fieldName: 'somefield', unique: true });
 }
-
-//function gotStream(stream) {
-//  console.log("Gotstream");
-//  video.src = webkitURL.createObjectURL(stream);
-//}
-
-/*function sourceChanged()
-{
-  var selectList = document.getElementById("sources");
-
-  console.log("Trying to turn on cam: " + sourceIDs[selectList.selectedIndex]);
-
-  //Try to set the video based on this source:
-  navigator.webkitGetUserMedia({video: {optional: [{sourceId: videoList.selectedIndex[0].value}]}},
-                               gotStream, function() {});
-}*/
-
-function gotSources(sourceInfos)
-{
-  var videoList = document.getElementById("video-list");
-  var audioList = document.getElementById("audio-list");
-
-  videoList.options.length = 0;
-  audioList.options.length = 0;
-
-  for (var i=0; i < sourceInfos.length; i++) {
-    console.log(sourceInfos[i])
-    if (sourceInfos[i].kind == 'video') {
-      videoList.options.add(new Option(sourceInfos[i].label), sourceInfos[i].id);
-    } else if (sourceInfos[i].kind == 'audio') {
-      audioList.options.add(new Option(sourceInfos[i].label), sourceInfos[i].id);
-    }
-  }
-}
-
-//video = document.getElementById("videofeed");
-
-//ffmpeg -f dshow -i video="Integrated Webcam":audio="Microphone (High Definition Aud" output.mkv
